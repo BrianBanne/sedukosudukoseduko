453fce8f9205c117ed676db20e6e5cef35c7f13d
shabazaps <shabazaps@gmail.com>
2024-03-07 20:58:48 +0530

    Added improved Sudoku Solver code

--- src/SedukoSolver.java
+++ src/SedukoSolver.java
@@ -1,24 +1,6 @@
 import java.util.stream.IntStream;
 
-import static java.lang.Integer.MIN_VALUE;
-;
-;
-;
-;
-;
-;
-;
-;
-// THIS IS NOT MY WORK \\
-;
-;
-;
-;
-;
-;
-;
-
-public class SedukoSolver {
+public class SudokuSolver {
 
     private static final int BOARD_SIZE = 9;
     private static final int SUBSECTION_SIZE = 3;
@@ -28,20 +10,19 @@ public class SedukoSolver {
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 9;
     private static int[][] board = {
-            { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
-            { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
-            { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
-            { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
-            { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
-            { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
-            { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
-            { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+        { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+        { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+        { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+        { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+        { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+        { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
     };
 
-
     public static void main(String[] args) {
-        SedukoSolver solver = new SedukoSolver();
+        SudokuSolver solver = new SudokuSolver();
         solver.solve(board);
         solver.printBoard();
     }
@@ -72,6 +53,7 @@ public class SedukoSolver {
         }
         return true;
     }
+
     private boolean isValid(int[][] board, int row, int column) {
         return rowConstraint(board, row) &&
                 columnConstraint(board, column) &&
@@ -88,7 +70,9 @@ public class SedukoSolver {
 
         for (int r = subsectionRowStart; r < subsectionRowEnd; r++) {
             for (int c = subsectionColumnStart; c < subsectionColumnEnd; c++) {
-                if (!checkConstraint(board, r, constraint, c)) return false;
+                if (!checkConstraint(board, r, constraint, c)) {
+                    return false;
+                }
             }
         }
         return true;
@@ -116,7 +100,4 @@ public class SedukoSolver {
         }
         return true;
     }
-
-
 }
-
